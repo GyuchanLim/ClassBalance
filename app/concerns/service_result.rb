3467d7c893da9a76b0f1e1cd5b56cc8f3053ee3b
gyuchan lim <gyuchantech@gmail.com>
2025-01-01 23:40:01 +1300

    Created ServiceResult concern
    Used to control the result of a service

---
+++ app/concerns/service_result.rb
@@ -0,0 +1,25 @@
+# Service result concern
+
+class ServiceResult
+  def initialize(was_successful, value: nil, errors: {})
+    @was_successful = was_successful
+    @value = value
+    @errors = errors
+  end
+
+  def self.success(value = nil)
+    new(true, value: value)
+  end
+
+  def self.failure(errors, value: nil)
+    new(false, errors: errors, value: value)
+  end
+
+  def success?
+    @was_successful
+  end
+
+  def failure?
+    !@was_successful
+  end
+end
